67d87c53ea80e6ad7713756522922c2b8586dcee
roboticsisamazingilovemyself <lliao26@moundsparkacademy.org>
2022-11-11 16:08:22 -0600

    TankDriveFirstCommit

---
+++ Lilly/tankdrive/src/main/java/frc/robot/commands/TankDrive.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class TankDrive extends CommandBase {
+
+  private final DoubleSupplier m_leftSpeed;
+  private final DoubleSupplier m_rightSpeed;
+  
+  private final DriveSubsystem m_DriveSubsystem;
+  
+  /** Creates a new TankDrive. */
+  public TankDrive(DriveSubsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ Lilly/tankdrive/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class DriveSubsystem extends SubsystemBase {
+  //create new motor object
+  private final WPI_TalonSRX motorFL = new WPI_TalonSRX (Constants.MOTOR_FL_ID);
+  private final WPI_TalonSRX motorBL = new WPI_TalonSRX (Constants.MOTOR_BL_ID);
+  private final WPI_TalonSRX motorFR = new WPI_TalonSRX (Constants.MOTOR_FR_ID);
+  private final WPI_TalonSRX motorBR = new WPI_TalonSRX (Constants.MOTOR_BR_ID);
+
+  DifferentialDrive differentialDrive = new DifferentialDrive(motorFL, motorFR);
+
+  /** Creates a new DriveSubsystem. */
+
+  public DriveSubsystem() {
+
+    // makes the back motors follow the front motors. 
+    motorBR.follow(motorFR);
+    motorBL.follow(motorFL);
+    // inverting motors
+    motorFR.setInverted(false); //to test
+    motorFL.setInverted(true); //to test
+    //set invert to the respective lead
+    motorBR.setInverted(InvertType.FollowMaster);
+    motorBL.setInverted(InvertType.FollowMaster);
+  }
+
+
+  @Override
+  public void periodic() {
+
+    // This method will be called once per scheduler run
+  }
+
+  public void setDriveSpeedTank (double leftSpeed, double rightSpeed){
+  //Speed limmiter 
+  leftSpeed = leftSpeed * 0.5;
+  rightSpeed = rightSpeed * 0.5;
+  //deadband makes small imputs read as 0
+  if (Math.abs(leftSpeed) < 0.1){leftSpeed = 0;}
+  if (Math.abs(rightSpeed) < 0.1){rightSpeed = 0;}
+   
+  differentialDrive.tankDrive(leftSpeed, rightSpeed);
+  
+    
+  }
+
+}
